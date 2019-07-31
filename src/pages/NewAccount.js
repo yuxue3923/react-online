5f001d0fcd0d7e879b3d2dfe7656b3fd975180a3
yusiyin <441382809@qq.com>
2019-07-31 10:46:43 +0800

    首页修改

--- src/pages/NewAccount.js
+++ src/pages/NewAccount.js
@@ -3,16 +3,19 @@ import {Link} from 'react-router-dom'
 import Swiper from 'swiper/dist/js/swiper.js'
 // import 'swiper/dist/css/swiper.min.css'
 import './NewAccount.css'
-import { Row, Col,PageHeader,Typography,Divider,Layout,Icon,Button, Card } from 'antd';
+import {Row, Col,PageHeader,Typography,Divider,Layout,Icon,Button, Card } from 'antd';
 import picurl from '../hd_logo.jpg'
 import pic1 from '.././Systemjpg/1.png'
 import pic2 from '.././Systemjpg/2.png'
 import pic3 from '.././Systemjpg/3.jpg'
 import pic4 from '.././Systemjpg/4.jpg'
 import pic5 from '.././Systemjpg/5.png'
+import { relative } from 'path';
 const { Title } = Typography;
 const { Text } = Typography;
 const { Header, Footer, Sider, Content } = Layout;
+const { Meta } = Card;
+
 
 
 class NewAccount extends Component {
@@ -31,6 +34,11 @@ class NewAccount extends Component {
     // 如果需要分页器
     pagination: {
       el: '.swiper-pagination',
+    },
+
+    autoplay:{
+      disableOninteraction:false,
+      delay:2000
     },
     
     // 如果需要前进后退按钮
@@ -57,32 +65,40 @@ class NewAccount extends Component {
           </Col>
         </Row>
       {/* </Row> */}
-      <Row>
+      
+      <Row style={{ background: "#1DA57A",  width: "100%" ,height:"1000px"}}>
         <Col span={16} >
-        <div class="swiper-container">
+        <div class="swiper-card">
+        <Card  >  
+        <div class="swiper-container">  
          <div class="swiper-wrapper">
-         
-         <div class="swiper-slide"> <img src={pic5} width="100%"/></div>
+         <div class="swiper-slide"> <img src={pic3} width="100%" /></div>
          <div class="swiper-slide"> <img src={pic1} width="100%"/></div>
          <div class="swiper-slide"> <img src={pic2} width="100%"/></div>
-         <div class="swiper-slide"> <img src={pic3} width="100%"/></div>
          <div class="swiper-slide"> <img src={pic4} width="100%"/></div>
+         <div class="swiper-slide"> <img src={pic5} width="100%"/></div>
          </div>
+         
+       {/* <-- 如果需要轮播小圆点 --> */}
+       <div class="swiper-pagination  swiper-pagination-bullets">
+       </div>
     
-    
-       {/* <-- 如果需要导航按钮 --> */}
-       <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
+        {/* <-- 如果需要导航按钮 --> */}
+       {/* <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>  */}
         </div>
-        </Col>
-        <Col span={8} >
-        <div className="Signup"><Link to='/Index/Register'><Button size="large" type="primary">注册</Button></Link></div>
-        <div className="Login"><Link to='/Index/Access'><Button size="large" type="primary">登录</Button></Link></div>
-        
+        </Card>
+        </div>
 
         </Col>
+        <Col span={8} >
+        <div className="Signup"><Link to='/Index/Register'><Button size="large" ><Icon type="smile" theme="twoTone" twoToneColor="#52c41a"/>注册</Button></Link></div>
+        <div className="Login"><Link to='/Index/Access'><Button size="large" ><Icon type="smile" theme="twoTone" twoToneColor="#52c41a"/>登录</Button></Link></div>
+        </Col>
       </Row>
+      
       </div>
+      
     )
   }
 }
